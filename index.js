44f56ea2fdd708cd8d91a616624e5f55b678b94e
ShovonTheBarua <shovonbarua0186@gmail.com>
2026-03-16 16:53:33 +0600

    env file added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require('cors')
+require('dotenv').config()
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = 3000;
 
+// middleware
+app.use(cors())
+app.use(express.json())
+
 const uri =
-  "mongodb+srv://car-rental:sXz80IDam5p1JKnN@cluster0.t257hoy.mongodb.net/?appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t257hoy.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,6 +27,10 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     await client.connect();
+
+
+
+    
 
     await client.db("admin").command({ ping: 1 });
     console.log(
