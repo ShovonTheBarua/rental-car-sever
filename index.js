954e8db2e7cc7f8461066d1124cbfb7ff69610eb
ShovonTheBarua <shovonbarua0186@gmail.com>
2026-03-17 13:08:55 +0600

    browse cars api added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = 3000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t257hoy.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t257hoy.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,16 +27,37 @@ async function run() {
   try {
     await client.connect();
 
+    const db = client.db("rentalCar");
+    const carsCollection = db.collection("Cars");
 
+    app.post("/cars", async (req, res) => {
+      const newCar = req.body;
+      const result = await carsCollection.insertOne(newCar);
+      res.send(result);
+    });
 
-    
+    app.get("/cars", async (req, res) => {
+      const result = await carsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/myListings", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.UserEmail = email;
+      }
+      console.log(query);
+      const cursor = carsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    
   }
 }
 
