0563343cde9a67d680fba3205d1c604302809944
ShovonTheBarua <shovonbarua0186@gmail.com>
2026-03-17 19:50:08 +0600

    card Details with id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 3000;
 
 // middleware
@@ -41,6 +41,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/carDetails/:id', async (req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await carsCollection.findOne(query)
+      res.send(result)
+    })
+ 
     app.get("/myListings", async (req, res) => {
       const email = req.query.email;
       const query = {};
